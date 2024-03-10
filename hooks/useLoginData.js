b8163197a068231737266c76a39b52b4f0c79c08
ElizPN <pankevych.liza@gmail.com>
2024-03-10 12:00:40 +0100

    add local storage and redirect

--- hooks/useLoginData.js
+++ hooks/useLoginData.js
@@ -1,10 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
 
 export const useLoginData = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false)
+  const router = useRouter(); 
+
+  useEffect(() => {
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+    if (isLoggedIn) {
+      router.push("/posts");
+    }
+  }, [router]); 
+  
 
   const handleLogin =   (e) => {
     e.preventDefault();
@@ -14,6 +26,8 @@ export const useLoginData = () => {
     if (username === "Liam" && password === "123123") {
       setTimeout(() => {
         console.log("Login successful");
+           localStorage.setItem("isLoggedIn", true);
+           router.push("/posts");
         setIsLoading(false);
     }, 1000);
 
---
+++ pages/posts.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PostsPage = () => {
+  return (
+    <div>
+      <h1>Posts Page</h1>
+      <p>Welcome to the Posts Page!</p>
+    </div>
+  );
+};
+
+export default PostsPage;
