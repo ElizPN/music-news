3c7093b18f7e7fb7a8f10c3ac9262d2433a0d94c
ElizPN <pankevych.liza@gmail.com>
2024-03-11 10:50:45 +0100

    place post cards in center

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import { usePostsData } from '../hooks/usePostsData'; 
-import './Posts.css'; 
+import React from "react";
+import { usePostsData } from "../hooks/usePostsData";
+import "./Posts.css";
 
 export const Posts = () => {
-const {posts} = usePostsData();
+  const { posts } = usePostsData();
 
   return (
     <div>
       <h2>Welcome Liam</h2>
-
-      {posts.map((post) => (
-        <div key={post.id} className="post-card">
-          <img src="/images/post.jpeg" alt={post.title} className="post-picture" />
-          <div className="post-content">
-            <h2 className="post-title">{post.title}</h2>
-            <p className='post-summary'>{post.body}</p>
+      <div className="post-card-container">
+        {posts.map((post) => (
+          <div key={post.id} className="post-card">
+            <img
+              src="/images/post.jpeg"
+              alt={post.title}
+              className="post-picture"
+            />
+            <div className="post-content">
+              <h2 className="post-title">{post.title}</h2>
+              <p className="post-summary">{post.body}</p>
+            </div>
           </div>
-        </div>
-      ))}
-
+        ))}
+      </div>
     </div>
   );
 };
-
