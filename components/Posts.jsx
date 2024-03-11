85fbe11e8ff867f314d340cbe0766b098e7990ec
ElizPN <pankevych.liza@gmail.com>
2024-03-11 10:40:05 +0100

    add body text, correct title

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -4,20 +4,17 @@ import './Posts.css';
 
 export const Posts = () => {
 const {posts} = usePostsData();
-console.log(posts, "state");
 
   return (
     <div>
-      <h1>Posts Page</h1>
-      <p>Welcome to the Posts Page!!!!</p>
+      <h2>Welcome Liam</h2>
 
       {posts.map((post) => (
         <div key={post.id} className="post-card">
           <img src="/images/post.jpeg" alt={post.title} className="post-picture" />
           <div className="post-content">
             <h2 className="post-title">{post.title}</h2>
-            <p className="post-summary">{post.summary}</p>
-            <button className="post-button">Read More</button>
+            <p className='post-summary'>{post.body}</p>
           </div>
         </div>
       ))}
