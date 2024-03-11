631730814f6afdd813e43244940f3e3417e40c3a
ElizPN <pankevych.liza@gmail.com>
2024-03-11 12:53:44 +0100

    reduce summary text

--- hooks/usePostsData.js
+++ hooks/usePostsData.js
@@ -6,6 +6,14 @@ const service = getPostsDataService();
 export const usePostsData = () => {
   const [posts, setPosts] = useState([]);
 
+  const truncateText = (text, maxLength) => {
+    if (text.length > maxLength) {
+      return text.substring(0, maxLength) + "...";
+    } else {
+      return text;
+    }
+  };
+
   useEffect(() => {
     // create async function because useEffect can not receive async function as param
     (async () => {
@@ -14,5 +22,5 @@ export const usePostsData = () => {
     })();
   }, []);
 
-  return { posts };
+  return { posts, truncateText };
 };
