2744a029f514c2e9e6c479672239ecd3f026b6d7
ElizPN <pankevych.liza@gmail.com>
2024-03-12 10:35:19 +0100

    add link to posts

--- components/SinglePost.jsx
+++ components/SinglePost.jsx
@@ -7,7 +7,9 @@ export const SinglePost = ({ post }) => {
         <h1 className="header">Header</h1>
       <div className="post-card-container">     
       <div className="post-card">
-      <button class="circle-button"></button>
+      <a href="../posts" >
+      <button  class="circle-button"></button>
+      </a>
         <h2 className="post-card">{post.title}</h2>
         <p className="post-summary">{post.body}</p>
         <img src="/images/post.jpeg" alt={post.title} className="post-picture" />
