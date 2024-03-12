ba0f921a674c7aeed984e81a3dae3ed6ebf41bf9
ElizPN <pankevych.liza@gmail.com>
2024-03-12 10:07:36 +0100

    add styling

--- components/SinglePost.jsx
+++ components/SinglePost.jsx
@@ -1,11 +1,20 @@
+import "./SinglePost.css";
+
 
 export const SinglePost = ({ post }) => {
     return (
-      <div>
-        <h2>{post.title}</h2>
-        <p>{post.body}</p>
-        <p>Date:</p>
-        <img src="/images/post.jpeg" alt={post.title} />
+        <div>
+        <h1 className="header">Header</h1>
+      <div className="post-card-container">     
+      <div className="post-card">
+      <button class="circle-button"></button>
+        <h2 className="post-card">{post.title}</h2>
+        <p className="post-summary">{post.body}</p>
+        <img src="/images/post.jpeg" alt={post.title} className="post-picture" />
+        </div>
+       
       </div>
+      </div>
+      
     );
   };
